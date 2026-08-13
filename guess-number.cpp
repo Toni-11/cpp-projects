cde8f54c38692bdaa8782e075a23213134ad63eb
Antton Mikhael <anttonmikhail5@gmail.com>
2026-08-13 21:18:41 +0300

    fix: correct guessing game win condition

--- guess-number.cpp
+++ guess-number.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <random>
+#include <limits>
 using namespace std;
+
 int main() {
-    srand(time(0));
-    int randomNumber = rand() % 100 + 1;
-    int number = 0;
-    cout << "Enter your guess Between (1:100)=";
-    cin >> number;
-    while (number < 1 || number > 100) { cout << " your number " << number << "out of range try again"; cout << "enter your guess between(1:100)="; cin >> number; }
-    if (number >= randomNumber + 25 || number <= randomNumber - 25) cout << "The Guess is false , Too far:";
-    else if (number <= randomNumber + 25 && number >= randomNumber - 25) cout << "The Guess is false , very close:";
-    else cout << "The Guess is True , you win!";
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<int> distribution(1, 100);
+    const int randomNumber = distribution(generator);
+    int number;
+
+    cout << "Guess the number between 1 and 100.\n";
+
+    while (true) {
+        cout << "Enter your guess: ";
+        cin >> number;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter a valid number.\n";
+            continue;
+        }
+
+        if (number < 1 || number > 100) {
+            cout << "Your number is out of range. Try again.\n";
+            continue;
+        }
+
+        if (number == randomNumber) {
+            cout << "Correct! You win!\n";
+            break;
+        }
+
+        int difference = number > randomNumber ? number - randomNumber : randomNumber - number;
+        if (difference > 25)
+            cout << "Too far.\n";
+        else
+            cout << "Very close. Try again.\n";
+    }
+
     return 0;
 }
